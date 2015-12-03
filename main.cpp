45369e156aa2432dea9efb8c39a46843d20cca6c
Jiri Zahradnik <Jirka.Zahradnik@email.cz>
2015-12-03 02:09:12 +0100

    modification of main.cpp

--- main.cpp
+++ main.cpp
@@ -6,32 +6,30 @@ using namespace std;
 
 int main() {
 
-    int years = 1;
-    int iterations = years * 365 / 42;
-    int chickenCount = 50000;
+    int chickenCount = 100;
 
     factory *factory1 = new factory();
 
-    for(int i = 0; i < iterations; i++){
-        vector<chicken> chick;
 
-        /* create chicks */
-        for (int i = 0; i < chickenCount; i++) {
-            chick.push_back(chicken());
-        }
+    vector<chicken> chick;
 
-
-        /* start simulation time */
-        for (int i = 0; i < 42; i++) {
-            factory1->nextDay(&chick);
-        }
-
-        factory1->statistics();
-
-        /* clean up chickens */
-        vector<chicken>().swap(chick);
-
+    /* create chicks */
+    for (int i = 0; i < chickenCount; i++) {
+        chick.push_back(chicken());
     }
+
+
+    /* start simulation time */
+    for (int i = 0; i < 42; i++) {
+        factory1->nextDay(&chick);
+    }
+
+    factory1->statistics();
+
+    /* clean up chickens */
+    vector<chicken>().swap(chick);
+
+
     /* clean up factory */
     delete factory1;
 
